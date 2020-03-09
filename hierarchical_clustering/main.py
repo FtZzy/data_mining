9bbf48b022ad98aabdadeb76646c0d1111c7bc1d
Nathan Krupa <nkrupa@edd.fr>
2020-03-09 10:46:23 +0100

    Add global variable for dim (cah)

--- hierarchical_clustering/main.py
+++ hierarchical_clustering/main.py
@@ -7,10 +7,11 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 import matplotlib.pyplot as plt
 
 SIZE_DATA = 20
+DIM_DATA = 2
 
 
 if __name__ == "__main__":
-    data = rand(SIZE_DATA, 2)
+    data = rand(SIZE_DATA, DIM_DATA)
 
     plt.figure()
     linked = linkage(data)
